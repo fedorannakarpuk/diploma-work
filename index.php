5cce3182a75385efd570e59ea21da29d94fec960
Anna <akarpuk007@gmail.com>
2020-01-07 22:04:42 +0300

    Просматривала информацию по регистрации и делала по шагово. Изменения вне папки Laravel.

--- index.php
+++ index.php
@@ -5,114 +5,36 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!--    т.к. в папке Laravel у нас подключены стили по бутстрапу.то в этом файле я отдельно подключаю иначе не работает-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="style/style.css">
-    <title>Diploma-work</title>
+    <title>Форма регистрации</title>
 </head>
 <body>
+<div class="container mt-4">
+<!--    Классы Bootstrap
+mt-4  == отступ сверху
+form-control ==
+btn btn-success == на кнопки стили
+-->
+    <h1> Форма регистрации  </h1>
+    <form action="check.php" method="post" class="reg">
 
-<table id="calendarBig">
-    <thead>
-    <tr><td><td><td>
-    <tbody>
-    <tr>
-        <td>
-            <table data-m="0">
-                <thead>
-                <tr><td colspan="7">Январь
-                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс
-                <tbody>
-            </table>
-        <td>
-            <table data-m="1">
-                <thead>
-                <tr><td colspan="7">Февраль
-                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс
-                <tbody>
-            </table>
-        <td>
-            <table data-m="2">
-                <thead>
-                <tr><td colspan="7">Март
-                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс
-                <tbody>
-            </table>
-    <tr>
-        <td>
-            <table data-m="3">
-                <thead>
-                <tr><td colspan="7">Апрель
-                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс
-                <tbody>
-            </table>
-        <td>
-            <table data-m="4">
-                <thead>
-                <tr><td colspan="7">Май
-                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс
-                <tbody>
-            </table>
-        <td>
-            <table data-m="5">
-                <thead>
-                <tr><td colspan="7">Июнь
-                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс
-                <tbody>
-            </table>
-    <tr>
-        <td>
-            <table data-m="6">
-                <thead>
-                <tr><td colspan="7">Июль
-                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс
-                <tbody>
-            </table>
-        <td>
-            <table data-m="7">
-                <thead>
-                <tr><td colspan="7">Август
-                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс
-                <tbody>
-            </table>
-        <td>
-            <table data-m="8">
-                <thead>
-                <tr><td colspan="7">Сентябрь
-                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс
-                <tbody>
-            </table>
-    <tr>
-        <td>
-            <table data-m="9">
-                <thead>
-                <tr><td colspan="7">Октябрь
-                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс
-                <tbody>
-            </table>
-        <td>
-            <table data-m="10">
-                <thead>
-                <tr><td colspan="7">Ноябрь
-                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс
-                <tbody>
-            </table>
-        <td>
-            <table data-m="11">
-                <thead>
-                <tr><td colspan="7">Декабрь
-                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс
-                <tbody>
-            </table>
-</table>
+        <input type="text" class="form-control" name="name" id="name" placeholder="Введите имя"> <br>
+        <input type="text" class="form-control" name="login" id="login" placeholder="Введите логин"> <br>
+        <input type="password" class="form-control" name="password" id="pass" placeholder="Введите пароль"> <br>
+        <button class="btn btn-success" type="submit">Зарегистрировать</button>
+
+    </form>
+
+
+
+</div>
 
 
 
 
 
-
-
-
-
-<script src="script/script.js"></script>
 
 </body>
 </html>
---
+++ index22.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Diploma-work</title>
+</head>
+<body>
+
+<!--<table id="calendarBig">-->
+<!--    <thead>-->
+<!--    <tr><td><td><td>-->
+<!--    <tbody>-->
+<!--    <tr>-->
+<!--        <td>-->
+<!--            <table data-m="0">-->
+<!--                <thead>-->
+<!--                <tr><td colspan="7">Январь-->
+<!--                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс-->
+<!--                <tbody>-->
+<!--            </table>-->
+<!--        <td>-->
+<!--            <table data-m="1">-->
+<!--                <thead>-->
+<!--                <tr><td colspan="7">Февраль-->
+<!--                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс-->
+<!--                <tbody>-->
+<!--            </table>-->
+<!--        <td>-->
+<!--            <table data-m="2">-->
+<!--                <thead>-->
+<!--                <tr><td colspan="7">Март-->
+<!--                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс-->
+<!--                <tbody>-->
+<!--            </table>-->
+<!--    <tr>-->
+<!--        <td>-->
+<!--            <table data-m="3">-->
+<!--                <thead>-->
+<!--                <tr><td colspan="7">Апрель-->
+<!--                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс-->
+<!--                <tbody>-->
+<!--            </table>-->
+<!--        <td>-->
+<!--            <table data-m="4">-->
+<!--                <thead>-->
+<!--                <tr><td colspan="7">Май-->
+<!--                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс-->
+<!--                <tbody>-->
+<!--            </table>-->
+<!--        <td>-->
+<!--            <table data-m="5">-->
+<!--                <thead>-->
+<!--                <tr><td colspan="7">Июнь-->
+<!--                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс-->
+<!--                <tbody>-->
+<!--            </table>-->
+<!--    <tr>-->
+<!--        <td>-->
+<!--            <table data-m="6">-->
+<!--                <thead>-->
+<!--                <tr><td colspan="7">Июль-->
+<!--                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс-->
+<!--                <tbody>-->
+<!--            </table>-->
+<!--        <td>-->
+<!--            <table data-m="7">-->
+<!--                <thead>-->
+<!--                <tr><td colspan="7">Август-->
+<!--                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс-->
+<!--                <tbody>-->
+<!--            </table>-->
+<!--        <td>-->
+<!--            <table data-m="8">-->
+<!--                <thead>-->
+<!--                <tr><td colspan="7">Сентябрь-->
+<!--                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс-->
+<!--                <tbody>-->
+<!--            </table>-->
+<!--    <tr>-->
+<!--        <td>-->
+<!--            <table data-m="9">-->
+<!--                <thead>-->
+<!--                <tr><td colspan="7">Октябрь-->
+<!--                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс-->
+<!--                <tbody>-->
+<!--            </table>-->
+<!--        <td>-->
+<!--            <table data-m="10">-->
+<!--                <thead>-->
+<!--                <tr><td colspan="7">Ноябрь-->
+<!--                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс-->
+<!--                <tbody>-->
+<!--            </table>-->
+<!--        <td>-->
+<!--            <table data-m="11">-->
+<!--                <thead>-->
+<!--                <tr><td colspan="7">Декабрь-->
+<!--                <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс-->
+<!--                <tbody>-->
+<!--            </table>-->
+<!--</table>-->
+
+
+
+
+
+
+
+
+
+<!--<script src="script/script.js"></script>-->
+
+</body>
+</html>
